d21c6a09b656b94c875af73c13291ece07b5c1c5
Muhamad Amir <m.amirazmi@hotmail.com>
2020-04-15 14:25:41 +0800

    9 done the my story page

--- src/components/MyStoryPage.js
+++ src/components/MyStoryPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavbarLanding from "./NavbarLanding";
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../img/profile.jpg";
+import logo from "../img/prof2.jpg";
 
 const MyStoryPage = (props) => {
   return (
@@ -9,6 +9,7 @@ const MyStoryPage = (props) => {
       <NavbarLanding />
       <Container>
         <h2>My Story</h2>
+        <hr />
         <Row>
           <Col xs={12} sm={4}>
             <div className="prof-img-box">
@@ -16,15 +17,27 @@ const MyStoryPage = (props) => {
             </div>
           </Col>
           <Col xs={12} sm={8}>
-            <div className="mystory-a">
-              <h4>Hi, I'm Muhamad Amir a self taught web developer...</h4>
+            <div className="mystory-text">
+              <h3>Hi, I'm Muhamad Amir a self taught web developer...</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Recusandae alias, nisi nulla deleniti ipsam dicta rerum quas
-                doloribus iure repudiandae. Aut dicta, ducimus voluptas
-                doloremque aliquid nobis debitis earum, eaque optio fuga
-                similique quasi vel ipsum. Natus incidunt omnis iure. Unde modi,
-                dolore soluta molestiae nemo doloribus hic eveniet recusandae.
+                My web development journey begins on November 2019. I love
+                programming during my study in Electronics Engineering with
+                plays around C and Assembly language, but I not really into
+                programming hardware. So, after graduation, I've start my own
+                self-study about web development with Javascript. Since then, I
+                have learned a lot of web development technology such as
+                <strong>NodeJS, MongoDB, Javascript, and React</strong>. This
+                Movement Control Order (MCO) really gives me a big chance to
+                <strong> learn and build</strong> as much as I can in order to
+                improve my skills. Currently learning the React framework as
+                well as integration between front end and back end. My goals is
+                to become a successful <strong>Full Stack Developer</strong>
+                which can provide a complete website solution to the client. To
+                begin with, now I'm seeking for a{" "}
+                <strong>Junior Web Developer</strong> or any{" "}
+                <strong>Internship in Web Development</strong> that can give me
+                a chance to show and improve my skills in the real world also
+                give chance for me to contribute my skills into a community.
               </p>
             </div>
           </Col>
